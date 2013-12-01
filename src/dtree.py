28f7b54adbfca416810c06b61914532348e87d15
Suchan Lee <suchanl@palantir.com>
2013-12-01 00:42:17 -0500

    first trial of decision tree

--- src/dtree.py
+++ src/dtree.py
@@ -5,6 +5,8 @@ from operator import itemgetter
 
 import numpy as np
 
+from parser import Parser
+
 
 class Node:
 	def __init__(self, left=None, right=None,
@@ -28,49 +30,43 @@ class Node:
 
 class DecisionTree:
 
-	def __init__(self, root=None):
-		self.root = root
+	def __init__(self):
+		self.root = None
+		self.keys = None
+		self.parser = Parser()
 
-	def decision_tree(data, depth):
+	def create_decision_tree_for_k(self, pos_data, neg_data, depth, attr, words_used):
 		'''
 		Trains and returns a decision tree with the information gain
 		as the tree splitting criterion. Criterion is a binary function
 		which checks to see if a word exists in the tweet or not
 		'''
+
 		root = Node(depth=depth)
-		data_len = len(data)
 
-		# some hack
-		dd = data[:]
-		dd.sort(key=itemgetter(1), reverse=True)
+		if self.root is None:
+			self.root = root
 
-		num_positive = np.sum(data, axis=0)[1]
-		if data_len == num_positive:
-			root.label = 1
-			return root
-		elif num_positive == 0:
+		if len(pos_data) == 0:
 			root.label = -1
 			return root
-		elif ceil(dd[0][0][0])-ceil(dd[data_len-1][0][0]) == 0:
-			if ceil(dd[0][0][1])-ceil(dd[data_len-1][0][1]) == 0:
-				if dd[0][1]-dd[data_len-1][1] != 0.0:
-					root.label = 1
-					return root
+		elif len(neg_data) == 0:
+			root.label = 1
+			return root
 
-		max_gain_info = max_gain(data)
-		root.criterion = max_gain_info
-		data_subset_1 = []
-		data_subset_2 = []
-		for i in range(data_len):
-			if data[i][0][max_gain_info[0]] >= max_gain_info[1]:
-				data_subset_1.append(data[i])
-			else:
-				data_subset_2.append(data[i])
-		root.left = decision_tree(data_subset_1, depth=depth+1)
-		root.right = decision_tree(data_subset_2, depth=depth+1)
-		return root
+		print 'Current depth: {}'.format(depth)
+		criterion_word, words_used = self.max_gain(pos_data, neg_data, words_used)
 
-	def entropy(data):
+		# cps = set positive tweets that contain the word
+		# cns = set negative tweets that contain the word
+		# ncps = set positive tweets that do not contain the word
+		# ncns = set negative tweets that do not contain the word
+		cps, ncps, cns, ncns = self.split_on_word(pos_data, neg_data, criterion_word)
+
+		root.left = self.create_decision_tree_for_k(ncps, ncns, depth+1, attr, words_used)
+		root.right = self.create_decision_tree_for_k(cps, cns, depth+1, attr, words_used)
+
+	def entropy(self, pos_data, neg_data):
 		'''
 		Calculates and returns the entropy of S (S=labels) with the
 		formula:
@@ -79,40 +75,70 @@ class DecisionTree:
 		if len(data) == 0:
 			return 0
 		else:
-			tot = float(len(data))
+			tot = float(len(pos_data) + len(neg_data))
 
-		num_positive = np.sum(data, axis=0)[1]
-		pos = num_positive
+		pos = len(pos_data)
 		if pos == 0:
 			pos = tot
-		neg = tot-num_positive
+		neg = tot-pos
 		if neg == 0:
 			neg = tot
 		return -(pos/tot)*log(pos/tot,2)-(neg/tot)*log(neg/tot,2)
 
 
-	def gain(data, target_attr, attr_val):
+	def gain(self, pos_data, neg_data, word):
 		'''
 		Calculates and returns the Gain(S,A) of an attribute A, with
 		relative to a collection of examples S
 		'''
-		subset_entropy = 0
-		data_subset_1 = [datum for datum in data if datum[0][target_attr] >= attr_val]
-		data_subset_2 = [datum for datum in data if datum[0][target_attr] < attr_val]
-		subset_entropy = (float(len(data_subset_1))/len(data))*entropy(data_subset_1)
-		subset_entropy += (float(len(data_subset_2))/len(data))*entropy(data_subset_2)
-		return entropy(data)-subset_entropy
+		set_entropy = self.entropy(pos_data, neg_data)
+		contains_pos_set, not_contains_pos_set, contains_neg_set, not_contains_neg_set = self.split_on_word(pos_data, neg_data, word)
+
+		len_contains = float(len(contains_pos_set) + len(contains_neg_set))
+		len_not_contains = float(len(not_contains_pos_set) + len(not_contains_neg_set))
+
+		contains_subset_entropy = self.entropy(contains_pos_set, contains_neg_set)*len_contains/(len_contains+len_not_contains)
+		not_contains_subset_entropy = self.entropy(not_contains_pos_set, not_contains_neg_set)*len_not_contains/(len_contains+len_not_contains)
+		subset_entropy = contains_subset_entropy + not_contains_subset_entropy
+		return set_entropy-subset_entropy
 
 
-	def max_gain(data):
-		max_gain_val = 0
-		max_gain_idx = [0,0]
-		for target_attr in range(len(data[0])):
-			for attr in range(10):
-				gain_val = gain(data, target_attr, attr)
-				if gain_val > max_gain_val:
-					max_gain_val = gain_val
-					max_gain_idx = [target_attr, attr]
-		return max_gain_idx
+	def max_gain(self, pos_data, neg_data, words_used):
+		max_gain = 0
+		max_word = None
+		for key in self.keys:
+			# print 'Trying key: {}'.format(key)
+			if key not in words_used:
+				gain = self.gain(pos_data, neg_data, key)
+				if gain > max_gain:
+					max_gain = gain
+					max_word = key
+		words_used.append(max_word)
+		return max_word, words_used
+
+	def split_on_word(self, pos_data, neg_data, word):
+		contains_pos_set = []
+		not_contains_pos_set = []
+		contains_neg_set = []
+		not_contains_neg_set = []
+		for i in range(len(pos_data)):
+			if word in pos_data[i][1]:
+				contains_pos_set.append(pos_data[i])
+			else:
+				not_contains_pos_set.append(pos_data[i])
+		for i in range(len(neg_data)):
+			if word in neg_data[i][1]:
+				contains_neg_set.append(neg_data[i])
+			else:
+				not_contains_neg_set.append(neg_data[i])
+		return contains_pos_set, not_contains_pos_set, contains_neg_set, not_contains_neg_set
+
+tree = DecisionTree()
+data = tree.parser.load_data('../data/train.csv')
+data = tree.parser.porter_stem_data(data)
+index, index_map = tree.parser.index_data(data)
+tree.keys = index.keys()
+pos_data, neg_data = tree.parser.get_label_divided_data(data, 'k1')
+tree.create_decision_tree_for_k(pos_data, neg_data, depth=1, attr='k1', words_used=[])
 
 
