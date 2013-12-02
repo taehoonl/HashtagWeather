18e3a5d5a22e9427f45c1c52ff2c6748a1832359
Taehoon <tl353@cornell.edu>
2013-12-02 01:22:38 -0500

    query finished... need to hook up google API

--- src/query_tweet.py
+++ src/query_tweet.py
@@ -25,11 +25,11 @@ class Query:
 	def count(self):
 		return len(self.tweet_data), self.coordinates, self.locations
 
-	def get_lat_lon(self, somestring):
+	def get_lon_lat(self, somestring):
 		# call google API for lat, lon
 		lat = 45.0
 		lon = 45.0
-		return math.radians(lat), math.radians(lon)
+		return lon, lat
 
 	def distance(self, lat1, lon1, lat2, lon2):
 		return math.acos( (math.sin(lat1)*math.sin(lat2)) + (math.cos(lat1)*math.cos(lat2)*math.cos(lon1-lon2)) )*self.R
@@ -43,18 +43,18 @@ class Query:
 			self.coordinates += 1
 			print 'coordinates : ', self.coordinates, tweet['coordinates']['coordinates']
 			# coord = map(lambda w: math.radians(float(w)), tweet['coordinates']['coordinates'].split(','))
-			e[0] = tweet['coordinates']['coordinates'][1] # latitude (in rad)
-			e[1] = tweet['coordinates']['coordinates'][0] # longitude (in rad)
+			e[0] = math.radians(tweet['coordinates']['coordinates'][1]) # latitude (in rad)
+			e[1] = math.radians(tweet['coordinates']['coordinates'][0]) # longitude (in rad)
 			# e[2] = math.sin(coord[0])
 			# e[3] = math.cos(coord[0])
 			e[2] = tweet['text']
 		# location
 		elif not (tweet['location'].strip() == ''):
 			self.locations += 1
-			print 'locations : ', self.locations, tweet['location']
-			lat, lon = self.get_lat_lon(tweet['location'].strip()) # (in rad)
-			e[0] = lat
-			e[1] = lon
+			print 'locations : ', self.locations, tweet['location'].strip()
+			lon, lat = self.get_lon_lat(tweet['location'].strip()) # (in rad)
+			e[0] = math.radians(lat)
+			e[1] = math.radians(lon)
 			# e[2] = math.sin(lat)
 			# e[3] = math.cos(lat)
 			e[2] = tweet['text']
@@ -64,18 +64,24 @@ class Query:
 
 		self.tweet_data.append(e)
 
-	def query_tweet(self, latitude, longitude, radius):
+	def query_coord(self, longitude, latitude, radius):
 		matches = []
 		lat_rad = math.radians(latitude)
 		lon_rad = math.radians(longitude)
+
 		for t in self.tweet_data:
 			dist = self.distance(lat_rad, lon_rad, t[0], t[1])
+			# print t[0], t[1], dist
 			if dist <= radius:
 				matches.append(t)
 		return matches
 
+	def query_address(self, address, radius):
+		lon, lat = self.get_lon_lat(address)
+		return self.query_coord(lon, lat, radius)
+
 
 if __name__ == '__main__':
-	q = Query('../twitDB.txt')
+	query = Query('../twitDB.txt')
 	pdb.set_trace()
 	print 'done'
